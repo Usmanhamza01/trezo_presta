a848ab2f3e180b8a63bc27c338fa3dd263874299
Usmanhamza01 <150922466+Usmanhamza01@users.noreply.github.com>
2026-08-07 00:21:02 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -10,6 +10,6 @@ window.TREZO_CONFIG = {
   url: "https://nxntmhqtrffbnjvozidt.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54bnRtaHF0cmZmYm5qdm96aWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTQwMjEsImV4cCI6MjA5ODkzMDAyMX0.YPqgHcwbtJ6jh-T2LXEFjI1CfgyGow1_BwbUOF0RaRk",
   edition: "services",
-  urlCommerce: "",
-  urlServices: ""
+  urlCommerce: "https://trezo-cloud.pages.dev/",
+  urlServices: "https://trezo-presta.pages.dev/"
 };
